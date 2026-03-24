13e581ecaf6ec728d3a6a88fd5bab178f9100f63
PapayaLabs <papayalabs@gmail.com>
2026-03-24 08:35:55 +0100

    Refactor migrations

--- db/migrate/20260306000001_create_tombstones.rb
+++
@@ -1,7 +0,0 @@
-class CreateTombstones < ActiveRecord::Migration[8.0]
-  def change
-    create_table "tombstones" do |t|
-      t.datetime "erected_at"
-    end
-  end
-end
--- db/migrate/20260306000003_create_users.rb
+++ db/migrate/20260306000003_create_users.rb
@@ -6,10 +6,14 @@ class CreateUsers < ActiveRecord::Migration[8.0]
       t.datetime "registered_at", default: -> { "CURRENT_TIMESTAMP" }
       t.string   "first_name", null: false
       t.string   "last_name"
+      t.string   "email"
+      t.string   "password_digest"
       t.jsonb    "preferences"
       t.bigint   "last_cancelled_message_id"
     end
 
+    add_index "users", ["email"],
+              name: "index_users_on_email", unique: true
     add_index "users", ["last_cancelled_message_id"],
               name: "index_users_on_last_cancelled_message_id"
   end
